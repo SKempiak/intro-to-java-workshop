fb0f6c2f08271ab571ef1b2e6e73adc528fa83ca
samus114 <league@WTS15.attlocal.net>
2017-07-28 11:45:52 -0700

    hi

--- IntroToJavaWorkshop-master 2/src/day5/StarShow.java
+++ IntroToJavaWorkshop-master 2/src/day5/StarShow.java
@@ -9,19 +9,19 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class StarShow {
 	
-	Robot robot = new Robot("batman");
+	Robot dog = new Robot("vic");
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+	//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
 
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+int xPos= 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+int yPos= 950;
 		// 8. Make a variable to hold the star size and set it to 25
-
+int i=25;
 		// 12. Repeat the steps #19 to #18, 30 times
 
 			// 19. Set the pen width to i 
@@ -46,13 +46,15 @@ public class StarShow {
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+dog.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
-
-			// 1. Move the robot the distance of the starSize variable
+for (int i = 0; i <5; i++) {
 	
+}
+			// 1. Move the robot the distance of the starSize variable
+	dog.move(150);
 			// 3. Turn the robot 144 degrees
-			
+			dog.turn(144);
 	}
 	
 	public static void main(String[] args) {
